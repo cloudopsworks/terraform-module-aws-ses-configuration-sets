16d3b23de4947c9719fd04256620fff5076470b8
Cristian Beraha <cristian@cloudops.works>
2025-03-21 14:59:14 -0300

    Update event destination name reference in SES configuration
    Revised the reference for `event_destination_name` to correctly point to `each.value.destination.name` instead of `each.value.name`. This ensures proper mapping and avoids potential misconfigurations in the SES event destinations.

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ resource "aws_sesv2_configuration_set" "this" {
 resource "aws_sesv2_configuration_set_event_destination" "this" {
   for_each               = local.event_destinations
   configuration_set_name = aws_sesv2_configuration_set.this[each.value.configset].configuration_set_name
-  event_destination_name = each.value.name
+  event_destination_name = each.value.destination.name
 
   event_destination {
     dynamic "cloud_watch_destination" {
