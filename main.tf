9d5171f5427522b7946e25da6437ebd23b19b639
Cristian Beraha <cristian@cloudops.works>
2025-03-21 13:49:46 -0300

    Add event destination resources for SES configuration sets
    This update introduces `aws_sesv2_configuration_set_event_destination` resources to support defining event destinations for SES configuration sets. The implementation uses a dynamic approach for handling multiple destination types, such as CloudWatch, EventBridge, Kinesis Firehose, SNS, and Pinpoint. Additionally, it ensures enhanced flexibility by leveraging Terraform locals and loops.

--- main.tf
+++ main.tf
@@ -4,9 +4,21 @@
 #            Distributed Under Apache v2.0 License
 #
 
+locals {
+  event_destinations = merge([
+    for key, config in var.configs : {
+      for k, dest in config : "${key}-${k}" => {
+        configset         = key
+        destinantion_name = k
+        destination       = dest
+      }
+    }
+  ]...)
+}
+
 resource "aws_sesv2_configuration_set" "this" {
   for_each               = var.configs
-  configuration_set_name = try(each.value.name, "") != "" ? each.value.name : format("%s-%s-configset", each.value.name_prefix, local.system_name)
+  configuration_set_name = try(each.value.name, "") != "" ? each.value.name : format("%s-%s-configset", try(each.value.name_prefix, each.key), local.system_name)
   dynamic "delivery_options" {
     for_each = length(try(each.value.delivery_options, {})) > 0 ? [each.value.delivery_options] : []
     content {
@@ -58,4 +70,50 @@ resource "aws_sesv2_configuration_set" "this" {
     }
   }
   tags = local.all_tags
+}
+
+resource "aws_sesv2_configuration_set_event_destination" "this" {
+  for_each               = local.event_destinations
+  configuration_set_name = aws_sesv2_configuration_set.this[each.value.configset].name
+  event_destination_name = each.value.destination_name
+
+  event_destination {
+    dynamic "cloud_watch_destination" {
+      for_each = length(try(each.value.destination.cloudwatch, {})) > 0 ? [1] : []
+      content {
+        dimension_configuration {
+          default_dimension_value = each.value.destination.cloudwatch.default_dimension_value
+          dimension_name          = each.value.destination.cloudwatch.dimension_name
+          dimension_value_source  = each.value.destination.cloudwatch.dimension_value_source
+        }
+      }
+    }
+    dynamic "event_bridge_destination" {
+      for_each = length(try(each.value.destination.event_bridge, {})) > 0 ? [1] : []
+      content {
+        event_bus_arn = each.value.destination.event_bridge.event_bus_arn
+      }
+    }
+    dynamic "kinesis_firehose_destination" {
+      for_each = length(try(each.value.destination.kinesis_firehose, {})) > 0 ? [1] : []
+      content {
+        delivery_stream_arn = each.value.destination.kinesis_firehose.delivery_stream_arn
+        iam_role_arn        = each.value.destination.kinesis_firehose.iam_role_arn
+      }
+    }
+    dynamic "sns_destination" {
+      for_each = length(try(each.value.destination.sns, {})) > 0 ? [1] : []
+      content {
+        topic_arn = each.value.destination.sns.topic_arn
+      }
+    }
+    dynamic "pinpoint_destination" {
+      for_each = length(try(each.value.destination.pinpoint, {})) > 0 ? [1] : []
+      content {
+        application_arn = each.value.destination.pinpoint.application_arn
+      }
+    }
+    enabled              = try(each.value.destination.enabled, false)
+    matching_event_types = try(each.value.destination.matching_types, [])
+  }
 }
