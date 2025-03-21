147d5a9eea8d2fe3a847767d2f69bacbbb61b14e
Cristian Beraha <cristian@cloudops.works>
2025-03-21 13:54:08 -0300

    Add outputs for SES configuration sets and event destinations
    This commit introduces outputs for SESv2 configuration sets and their event destinations. These outputs include details such as ARNs, configuration set names, destination names, and IDs, enhancing module usability and integration.

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,20 @@
 #            Distributed Under Apache v2.0 License
 #
 
+output "configuration_sets" {
+  value = {
+    for k, v in aws_sesv2_configuration_set.this : k => {
+      arn = v.arn
+    }
+  }
+}
+
+output "event_destinations" {
+  value = {
+    for k, v in aws_sesv2_configuration_set_event_destination.this : k => {
+      configuration_set_name = v.configuration_set_name
+      destinantion_name      = v.destinantion_name
+      id                     = v.id
+    }
+  }
+}
